8d536ee6ba28522d11618c4155a3b2b2170043c4
Pedro Vitachi <pedrohenriquevitachi@gmail.com>
2025-08-04 13:25:51 -0300

    Update Tables.sql

--- Dados/Tables.sql
+++ Dados/Tables.sql
@@ -1,22 +1,27 @@
 create database music;
 use music;
 
-CREATE TABLE `artists`(
-	`name` VARCHAR(50) NOT NULL,
-	`is_group` VARCHAR(20) NOT NULL
+CREATE TABLE `artists` (
+    `name` VARCHAR(50) NOT NULL,
+    `is_group` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`name`)
 );
 
 CREATE TABLE `albums` (
-	`name` VARCHAR(50) NOT NULL,
-	`release_year` INT NOT NULL,
-	`duration` TIME NOT NULL,
-	`no_tracks` INT NOT NULL
+    `name` VARCHAR(75) NOT NULL,
+    `release_year` INT NOT NULL,
+    `duration` TIME NOT NULL,
+    `no_tracks` INT NOT NULL,
+    PRIMARY KEY (`name`)
 );
 
 CREATE TABLE `songs` (
-	`name` VARCHAR(50) NOT NULL,
-	`artist_name` VARCHAR(50) NOT NULL,
-	`feat_name` VARCHAR(75),
-	`album_name` VARCHAR(75),
-	`from_country` VARCHAR(30) NOT NULL
+    `name` VARCHAR(50) NOT NULL,
+    `artist_name` VARCHAR(50) NOT NULL,
+    `feat_name` VARCHAR(75),
+    `album_name` VARCHAR(75),
+    `from_country` VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`name`),
+    FOREIGN KEY (`artist_name`) REFERENCES `artists`(`name`),
+    FOREIGN KEY (`album_name`) REFERENCES `albums`(`name`)
 );
