8f24ae2afc6e62f8bf4436cf22dfc3b0a2820e9c
Pedro Vitachi <pedrohenriquevitachi@gmail.com>
2025-08-03 23:54:46 -0300

    Delete Shortest_song_names.sql

--- Shortest_song_names.sql
+++
@@ -1,6 +0,0 @@
-SELECT `artists`.`name` AS 'Artists with the shortest songs names',
-`songs`.`name` AS 'Song name'
-FROM `artists` 
-JOIN `songs` ON `songs`.`artist_name` = `artists`.`name`
-ORDER BY LENGTH(`songs`.`name`) ASC
-LIMIT 10;
