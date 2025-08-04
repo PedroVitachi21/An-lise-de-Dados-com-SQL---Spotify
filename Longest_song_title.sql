d83ab2b390f056dbfe9fcd1e08158a6ae292757a
Pedro Vitachi <pedrohenriquevitachi@gmail.com>
2025-08-03 23:54:38 -0300

    Delete Longest_song_title.sql

--- Longest_song_title.sql
+++
@@ -1,8 +0,0 @@
-SELECT `artists`.`name` AS 'Artist with the longest song name',
-`songs`.`name` AS 'Song name'
-FROM `artists` 
-JOIN `songs` ON `songs`.`artist_name` = `artists`.`name`
-WHERE LENGTH(`songs`.`name`) = (
-				SELECT MAX(LENGTH(`songs`.`name`))
-                FROM `songs`
-);
