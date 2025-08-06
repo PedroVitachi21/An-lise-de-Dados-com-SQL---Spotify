e0ef0397a38a2e24cdfdbe982fee1a93fcc283a2
Pedro Vitachi <pedrohenriquevitachi@gmail.com>
2025-08-06 14:25:34 -0300

    Update Albums_brazil_less_fifty_minutes.sql

--- Queries/Albums_brazil_less_fifty_minutes.sql
+++ Queries/Albums_brazil_less_fifty_minutes.sql
@@ -2,9 +2,9 @@ SELECT DISTINCT `albums`.`name` AS 'Name of the album',
 `artists`.`name` AS 'Name of the artist',
 `albums`.`duration` AS 'Running time', 
 `songs`.`from_country` AS 'Country of origin'
-FROM `songs`
-JOIN `artists` ON `artists`.`name` = `songs`.`artist_name`
-JOIN `albums` ON `albums`.`name` = `songs`.`album_name`
-WHERE `songs`.`from_country` = 'Brasil'
+FROM `songs` 
+JOIN `albums` ON `albums`.`id` = `songs`.`album_id`
+JOIN `artists` ON `artists`.`id` = `songs`.`artist_id`
+WHERE `songs`.`from_country` = 'BRA'
 AND `albums`.`duration` < '00:50:00'
 ORDER BY `albums`.`duration` DESC;
