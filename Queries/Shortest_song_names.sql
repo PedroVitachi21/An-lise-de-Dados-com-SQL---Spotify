3036baaf60824c069517dbae19fb6dc0079440fb
Pedro Vitachi <pedrohenriquevitachi@gmail.com>
2025-08-06 14:26:38 -0300

    Update Shortest_song_names.sql

--- Queries/Shortest_song_names.sql
+++ Queries/Shortest_song_names.sql
@@ -1,6 +1,6 @@
 SELECT `artists`.`name` AS 'Artists with the shortest songs names',
 `songs`.`name` AS 'Song name'
 FROM `artists` 
-JOIN `songs` ON `songs`.`artist_name` = `artists`.`name`
+JOIN `songs` ON `songs`.`artist_id` = `artists`.`id`
 ORDER BY LENGTH(`songs`.`name`) ASC
 LIMIT 10;
