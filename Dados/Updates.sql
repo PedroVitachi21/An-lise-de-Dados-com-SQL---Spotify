747b4f34d5701684b9f6f681d59d0f60c9beab11
Pedro Vitachi <pedrohenriquevitachi@gmail.com>
2025-08-06 14:22:16 -0300

    Update Updates.sql

--- Dados/Updates.sql
+++ Dados/Updates.sql
@@ -2,13 +2,10 @@ UPDATE `songs`
 SET `feat_name` = 'No features'
 WHERE `feat_name` = '';
 
-UPDATE `songs`
-SET `album_name` = 'Single'
-WHERE `album_name` = '';
-
-UPDATE `songs`
-SET `album_name` = '8 Mile (Music From and Inspired By The Motion Picture)'
-WHERE `album_name` = '8 Mile';
+UPDATE `albums`
+JOIN `songs` ON `songs`.`album_id` = `albums`.`id`
+SET `albums`.`name` = 'Single'
+WHERE `songs`.`album_id` IS NULL;
 
 UPDATE `artists`
 SET `is_group` = 'Solo artist'
@@ -19,9 +16,9 @@ SET `is_group` = 'Group of artists'
 WHERE `is_group` = '1';
 
 UPDATE `songs`
-SET `from_country` = 'United States'
+SET `from_country` = 'USA'
 WHERE `from_country` = 'Estados Unidos';
 
 UPDATE `songs`
-SET `from_country` = 'Brazil'
+SET `from_country` = 'BRA'
 WHERE `from_country` = 'Brasil';
