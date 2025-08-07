20894cd75951825ffc9bc1900ef8b1194d8dd99a
Pedro Vitachi <pedrohenriquevitachi@gmail.com>
2025-08-07 01:52:59 -0300

    Atualizar o Artists_number_songs.sql

--- Queries/Artists_number_songs.sql
+++ Queries/Artists_number_songs.sql
@@ -1,7 +1,6 @@
 SELECT `artists`.`name` AS 'Artist/Group name',
 COUNT(`songs`.`name`) AS 'Ammount of songs'
 FROM `songs` 
-JOIN `albums` ON `albums`.`id` = `songs`.`album_id`
 JOIN `artists` ON `artists`.`id` = `songs`.`artist_id`
 GROUP BY `artists`.`name`
 ORDER BY COUNT(`songs`.`name`) DESC;
